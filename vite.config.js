4b1ce5b8e46d255a8a90a6f87e441af2fb23713c
Lalit Gattani <lalitgattani139@gmail.com>
2025-07-25 13:54:44 +0530

    Fix: Vercel routing and cleanUrls

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ Add this line
+  base: "/",
+  build: {
+    outDir: "dist"
+  }
 })
